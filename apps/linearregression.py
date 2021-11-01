9b268d254656297bf21c5bbf8f7692d0ea5f0cc1
Ken Harmon <harmkenn@gmail.com>
2021-10-31 20:06:44 -0600

    PSTv21

--- apps/linearregression.py
+++ apps/linearregression.py
@@ -1,5 +1,100 @@
 import streamlit as st
+import pandas as pd
+import scipy as sp
+import numpy as np
+from plotnine import *
+import pingouin as pg
+
 
 def app():
     # title of the app
-    st.title("Linear Regression")
+    st.subheader("Linear Regression") 
+    lrplot = ggplot() + coord_fixed(ratio = .1)
+    c1,c2 = st.columns(2)
+    with c1:
+        gs_URL = st.text_input("Public Google Sheet URL:","https://docs.google.com/spreadsheets/d/1Fx7f6rM5Ce331F9ipsEMn-xRjUKYiR3R_v9IDBusUUY/edit#gid=0") 
+        googleSheetId = gs_URL.split("spreadsheets/d/")[1].split("/edit")[0]
+        worksheetName = st.text_input("Sheet Name:","Bivariate")
+        URL = f'https://docs.google.com/spreadsheets/d/{googleSheetId}/gviz/tq?tqx=out:csv&sheet={worksheetName}'
+        #@st.cache (ttl = 600)
+        def upload_gs(x):
+            out = pd.read_csv(x)
+            return out
+
+        df = upload_gs(URL)
+        df = df.dropna(axis=1, how="all")
+        st.dataframe(df.assign(hack='').set_index('hack')) 
+        global numeric_columns
+        global non_numeric_columns
+        numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
+        non_numeric_columns = list(df.select_dtypes(['object']).columns)
+        non_numeric_columns.append(None)
+        non_numeric_columns.reverse()
+        st.sidebar.markdown('Regression Settings')
+        xvar = st.sidebar.selectbox('X-Axis', options=numeric_columns)
+        yvar = st.sidebar.selectbox('Y-Axis', options=numeric_columns)
+        cat = st.sidebar.selectbox('Category', options=non_numeric_columns)
+        if cat != None:
+            allcat = list(df[cat].unique())
+            cat1 = st.sidebar.selectbox('Variable',options=allcat)
+            sdf = df[[xvar,yvar,cat]]
+            fsdf = sdf[sdf[cat]==cat1]
+        if cat == None:
+            fsdf = sdf = df[[xvar,yvar]]
+    with c2:
+        if xvar == yvar:
+            st.warning("Select different X-Axis and Y-Axis")
+        if xvar != yvar:
+            st.markdown(f"X-Axis: {xvar}")
+            st.markdown(f"Y-Axis: {yvar}")
+            if cat != None:
+                st.markdown(f"Category: {cat}")   
+                st.markdown(f"Variable: {cat1}")  
+            fsdfs = fsdf.describe()
+            st.dataframe(fsdfs.iloc[0:3,:])
+            slope, intercept, r, p, ses = sp.stats.linregress(fsdf[xvar],fsdf[yvar])
+            fsdf['predy']=intercept+slope*fsdf[xvar]
+            fsdf['resid']=fsdf[yvar]-fsdf['predy']
+            fsdf['sqresid']=fsdf['resid']**2
+            sr = np.sqrt(fsdf['sqresid'].sum()/(fsdf['sqresid'].count()-2))
+            data = pd.DataFrame({"r":r,'R-Squared':r**2,'se (resid)':sr,'Intercept':intercept,'Slope':slope,'se (slope)':ses,'t (slope)':slope/ses,'p (slope)':p},index = [0]).T 
+            st.write(data)
+            lrplot = ggplot(aes(x=fsdf[xvar],y=fsdf[yvar])) + coord_fixed(ratio = .1)
+            lrplot = lrplot + geom_point(color = 'steelblue') + stat_smooth(method='lm') 
+             
+    st.markdown('''---''')
+    d1,d2 = st.columns(2)
+    with d1:
+        lr_graph = st.selectbox('Graph',["Scatter Plot","Residual QQPlot"])
+        if lr_graph == "Scatter Plot":
+            
+            st.pyplot(ggplot.draw(lrplot)) 
+        if lr_graph == "Residual QQPlot":
+            p = pg.qqplot(fsdf['resid'], dist='norm')
+            st.pyplot(ggplot.draw(p))
+            ddd = sp.stats.shapiro(fsdf['resid'])[1]
+            st.write("Shapiro p-Value: " + str(ddd))    
+    with d2:
+        gx =  float(st.text_input("Given X:",0)) 
+        py = intercept + slope*gx
+        st.markdown("Predicted y: "+str(py))  
+        alpha = float(st.text_input("alpha",.05))
+        cl = 1-alpha
+        n = len(fsdf)
+        meanx = np.mean(fsdf[xvar])
+        sdx = np.std(fsdf[xvar])
+        dof = n-2
+        ctv = sp.stats.t.ppf(alpha/2,dof) 
+        slower = slope + ctv*ses
+        shigher = slope - ctv*ses
+        st.markdown(str(100*cl)+"'%' confidnce interval for slope: ("+str(slower)+","+str(shigher)+")")
+        seyhat = sr*np.sqrt(1+1/n+(gx-meanx)**2/((n-1)*sdx**2))
+        pime = ctv*seyhat
+        plower = py + pime
+        phigher = py - pime         
+        st.markdown(str(100*cl)+"'%' prediction interval for y when x = "+str(gx)+": ("+str(plower)+","+str(phigher)+")")
+        semuhat = sr*np.sqrt(1/n+(gx-meanx)**2/((n-1)*sdx**2))
+        cimy = ctv*semuhat
+        clower = py + cimy
+        chigher = py - cimy 
+        st.markdown(str(100*cl)+"'%' confidence interval for mean y when x = "+str(gx)+": ("+str(clower)+","+str(chigher)+")")
--- multiapp.py
+++ multiapp.py
@@ -39,7 +39,7 @@ class MultiApp:
         })
 
     def run(self):
-        st.sidebar.title("Python Stat Tools v2.0")
+        st.sidebar.title("Python Stat Tools v2.1")
         st.sidebar.subheader("by Ken Harmon")
         
         app = st.sidebar.radio(
