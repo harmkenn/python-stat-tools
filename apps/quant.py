536800bd07e400d1e1b8733630ed4473fd1a7fac
Ken Harmon <harmkenn@gmail.com>
2022-02-25 09:29:19 -0700

    PST v 3.1

--- apps/quant.py
+++ apps/quant.py
@@ -11,7 +11,7 @@ def app():
     # Add a sidebar
     st.sidebar.subheader("Graph Settings")
     
-    top = st.columns((1,2))
+    top = st.columns((1,1))
     bottom = st.columns(1)
     with top[0]:
         gs_URL = st.session_state.gs_URL
@@ -46,47 +46,47 @@ def app():
     p = ggplot(df)
     
     if chart_choice == "Histogram":
-        st.sidebar.subheader("Histogram Settings")
-        x = st.sidebar.selectbox('X-Axis', options=numeric_columns)
-        cv = st.sidebar.selectbox("Color", options=non_numeric_columns)
-        bins = st.sidebar.slider("Number of Bins", min_value=1,max_value=40, value=7)
+        with top[1]:
+            x = st.selectbox('X-Axis', options=numeric_columns)
+            cv = st.selectbox("Color", options=non_numeric_columns)
+            bins = st.slider("Number of Bins", min_value=1,max_value=40, value=7)
         if cv != None:
             p = p + geom_histogram(aes(x=x, fill = cv, color = cv),position= "identity",alpha=.4, bins = bins)
         else:
             p = p + geom_histogram(aes(x=x),color="darkblue", fill="lightblue", bins = bins)
             
     if chart_choice == "Boxplot":
-        st.sidebar.subheader("Boxplot Settings")
-        x = st.sidebar.selectbox('X-Axis', options=numeric_columns)
-        cv = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        with top[1]:
+            x = st.selectbox('X-Axis', options=numeric_columns)
+            cv = st.selectbox("Color", options=non_numeric_columns)
         if cv != None:
             p = p + geom_boxplot(aes(x=cv,y=x, fill = cv)) + coord_flip()
         else:
             p = p + geom_boxplot(aes(x=1,y=x,width=.1),color="darkblue", fill="lightblue") + coord_flip()
             
     if chart_choice == "Dotplot":
-        st.sidebar.subheader("Dotplot Settings")
-        x = st.sidebar.selectbox('X-Axis', options=numeric_columns)
-        cv = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        with top[1]:
+            x = st.selectbox('X-Axis', options=numeric_columns)
+            cv = st.selectbox("Color", options=non_numeric_columns)
         if cv != None:
             p = p + geom_jitter(aes(x=cv,y=x, fill = cv, color = cv), size = 2, height = 0, width =.1)+ coord_flip()
         else:
             p = p + geom_jitter(aes(x=1,y=x), size = 2, height = 0, width =.1)+ coord_flip()
             
     if chart_choice == "QQplot":
-        st.sidebar.subheader("QQplot Settings")
-        x = st.sidebar.selectbox('X-Axis', options=numeric_columns)
-        cv = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        with top[1]:
+            x = st.selectbox('X-Axis', options=numeric_columns)
+            cv = st.selectbox("Color", options=non_numeric_columns)
         if cv != None:
             p = p + stat_qq(aes(sample=x,color=cv)) + stat_qq_line(aes(sample=x,color=cv))+ labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
         else:
             p = p + stat_qq(aes(sample=x)) + stat_qq_line(aes(sample=x)) + labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
             
     if chart_choice == "Scatterplot":
-        st.sidebar.subheader("Scatterplot Settings")
-        x = st.sidebar.selectbox('X-Axis', options=numeric_columns)
-        y = st.sidebar.selectbox('Y-Axis', options=numeric_columns)
-        cv = st.sidebar.selectbox("Color", options=non_numeric_columns)
+        with top[1]:
+            x = st.selectbox('X-Axis', options=numeric_columns)
+            y = st.selectbox('Y-Axis', options=numeric_columns, index = 1)
+            cv = st.selectbox("Color", options=non_numeric_columns)
         if cv != None:
             p = p + geom_point(aes(x=x,y=y,color=cv))
         else:
