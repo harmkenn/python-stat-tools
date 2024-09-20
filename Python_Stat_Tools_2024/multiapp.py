29853953e0e0e42ef2a51dd4fdc26450ebee425a
Ken Harmon <harmkenn@gmail.com>
2024-09-20 19:50:07 +0200

    Update

--- Python_Stat_Tools_2024/multiapp.py
+++ Python_Stat_Tools_2024/multiapp.py
@@ -35,7 +35,7 @@ class MultiApp:
         if st.session_state.xlsx is None: st.session_state.xlsx = r"Default.xlsx"
         #st.session_state.xlsx = st.sidebar.text_input("Enter the path to your .xlsx file:",r"PythonStatsData.xlsx")
         
-        st.sidebar.download_button(label="Download Default Excel File", data=open("Python_Sts_Tools_2024/PythonStatsData.xlsx", "rb").read(), file_name="PythonStatsData.xlsx")
+        st.sidebar.download_button(label="Download Default Excel File", data=open("Python_Stat_Tools_2024/PythonStatsData.xlsx", "rb").read(), file_name="PythonStatsData.xlsx")
 
            
         app = st.sidebar.radio(
